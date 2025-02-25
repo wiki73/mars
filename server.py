7651750a196f7bfa0a613d40d155df15b5814003
DANIIL-PRO-123 <gossee884@gmail.com>
2025-02-25 17:23:25 +0300

    первая задча

--- server.py
+++ server.py
@@ -1,1 +1,15 @@
-fdfsd
+from flask import Flask, request, url_for,  render_template, json,redirect
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    user = "Ученик Яндекс.Лицея"
+    return render_template('base.html', title='title')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
